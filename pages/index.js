2ab4dfa49ac5668687681841cf9351b708cb13a9
hallmaker <hallmaker32@gmail.com>
2024-06-25 20:22:35 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Coming Soon!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
