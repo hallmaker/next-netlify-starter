85851917ce28d6abecc6131a89f465416b020ca9
hallmaker <hallmaker32@gmail.com>
2024-06-25 20:24:03 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,6 @@ export default function Home() {
 
       <main>
         <Header title="Coming Soon!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
